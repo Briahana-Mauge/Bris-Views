3c37f9bbcb67db0e8f8cf9dc91cf30015073e660
Bpmauge <briahanamauge@pursuit.org>
2020-01-19 21:25:20 -0500

    this component is the form for comment submission

---
+++ youtube/src/Components/CommentForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CommentForm = (props) =>{
+        return (
+            <>
+            <h3>Leave A Comment!</h3>
+            <form onSubmit = {props.handleSubmit}>
+                <input type='text' placeholder='name' onChange = {props.handleNameChange} required/>
+                <input type='text' placeholder='comment' onChange = {props.handleCommentChange} required/>
+                <input type='submit' value='Submit' />
+            </form>
+            </>
+        )
+}
+
+export default CommentForm;
