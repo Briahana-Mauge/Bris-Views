b83fe9220de74d51246a15009e1e7df6d33b2385
Bpmauge <briahanamauge@pursuit.org>
2020-02-03 18:13:19 -0500

    added home page

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Link, Route, Switch } from 'react-router-dom';
 import About from './Components/About';
+import Search from './Components/Search';
 import Home from './Components/Home';
 import Video from './Components/Video';
 // import ErrorNotFound
@@ -12,8 +13,9 @@ class App extends React.Component {
       <div className="App">
 
         <nav>
-          Youtube
-        <Link to='/'> Home</Link> {' '}
+          
+        <Link to='/'> Youtube</Link> {' '}
+        <Link to='/search'> Search</Link> {' '}
         <Link to='/about'>About</Link>{' '}
 
         </nav>
@@ -22,6 +24,7 @@ class App extends React.Component {
 
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route path='/search' component={Search} />
           <Route path='/video' component={Video} />
           <Route path='/about' component={About} />
         </Switch>
