b53a2dcf0f542b0f02aa54e703fc81dce5534a4c
Bpmauge <briahanamauge@pursuit.org>
2020-02-03 18:14:43 -0500

    create history page

---
+++ youtube/src/Components/History.jsx
@@ -0,0 +1,6 @@
+import api from '../secret.js'
+
+https://developers.google.com/youtube/v3/docs/videos/list?apix=true&apix_params=%7B%22part%22%3A%22snippet%2CcontentDetails%2Cstatistics%22%2C%22id%22%3A%22Lp6W4aK1sbs%22%7D
+
+
+GET `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${id}&key=${api}`
