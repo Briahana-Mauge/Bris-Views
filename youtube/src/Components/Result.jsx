b062d0c657d94e881ab2178a5584bb252533c27d
Bpmauge <briahanamauge@pursuit.org>
2020-02-03 18:11:54 -0500

    fixed prop from id to result

--- youtube/src/Components/Result.jsx
+++ youtube/src/Components/Result.jsx
@@ -1,61 +1,62 @@
- import React from 'react';
+import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import Video from './Video';
+
 class Result extends React.Component {
     constructor(props) {
         super();
         this.state = {
-            results: props.ids
+            results: props.results
         }
     }
     renderVideos = () => {
         let { results } = this.state
         return (
-            
-                    results.map(result => {
-                        return (
-                           
-                                <Link to={`/video/${result.id}`}>
-                                    {
-                                        <>
-                                        <div className = 'singleVid'>
-                                        <img
+            results.map(result => {
+                return (
+
+                    <Link to={`/video/${result.id}`} key={result.id}>
+                        {
+                            <>
+                                <div className='singleVid'>
+                                    <img
                                         key={result.id}
-                                        alt = {result.title}
-                                            src={result.thumbnail}
-                                            width={result.width}
-                                            height={result.height}
-                                        >
-                                        </img>
-                                        <h3>{result.title}</h3>
-                                        </div>
-                                    </>
-                                }
-                                </Link>
-                    
-                        )
-                    })
+                                        alt={result.title}
+                                        src={result.thumbnail}
+                                        width={result.width}
+                                        height={result.height}
+                                    >
+                                    </img>
+                                    <h3>{result.title}</h3>
+                                </div>
+                            </>
+                        }
+                    </Link>
+
+                )
+            })
         )
     }
     singleVideo = () => {
-        const {results} = this.state
+        const { results} = this.state
+        // this.props.receiveHistory(results)
         return (
             <>
-              <Video id = {results}/>
+                <Video id={results} />
             </>
         )
     }
     render() {
         return (
-            <><div id = 'results'>
+            <>
+            <div id='results'>
                 <Switch>
-                    <Route exact path='/' render={this.renderVideos} />
+                    <Route path='/' render={this.renderVideos} />
                     <Route path='/video/:id' render={this.singleVideo} />
                 </Switch>
-                </div>
+            </div>
             </>
         )
     }
 }
 export default Result;
-
