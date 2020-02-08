6d80715a75b641d410c9e393f47ca9fa6005e7dd
Bpmauge <briahanamauge@pursuit.org>
2020-02-08 04:00:14 -0500

    trying new api key

--- youtube/src/Components/Home.jsx
+++ youtube/src/Components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Result from './Result';
-import api from '../secret.js'
+// import api from '../secret.js'
 
 class Home extends React.Component {
     constructor() {
@@ -17,7 +17,8 @@ class Home extends React.Component {
         const { search } = this.state;
         let videos = [];
         let newResults = []
-
+        let api = process.env.API_KEY
+        
         let searchAPI = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=8&order=relevance&key=${api}&q=${search}&type=video`
         try {
             let res = await axios.get(searchAPI)
