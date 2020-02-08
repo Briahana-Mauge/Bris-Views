eba62a905136f661971e241cdc387b13d99b1dcb
Bpmauge <briahanamauge@pursuit.org>
2020-02-08 03:10:18 -0500

    got rid of old files and added package.jsaon with heroku scripts

--- db/db.js
+++
@@ -1,6 +0,0 @@
-const pgp = require('pg-promise')();
-// const connectionString = 'postgres://localhost:5432/youtube';
-const connectionString = process.env.DATABASE_URL
-const db = pgp(connectionString);
-
-module.exports = db;
