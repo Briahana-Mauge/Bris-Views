d5d6abb814989c759cc7c372eda61855287c0055
Bpmauge <briahanamauge@pursuit.org>
2020-02-08 02:22:24 -0500

    set up server to serve our built react app

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var session = require('express-session')
 var passport = require('./auth/passport')
 
-var indexRouter = require('./routes/index');
+
 var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
 var historyRouter = require('./routes/history');
@@ -19,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../youtube/build')));
+
 app.use(session({
   secret: "NOT_A_GOOD_SECRET",
   resave: false,
@@ -29,9 +31,14 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 // Routes
-app.use('/', indexRouter);
+// app.use('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../youtube/build/index.html'))
+// })
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 app.use('/history', historyRouter);
 
+app.use('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../youtube/build/index.html'))
+})
 module.exports = app;
